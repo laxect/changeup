b3b60a5fe88e24e82f6b8a7e8016836ad638fc1d
gyara <me@gyara.moe>
2022-03-27 21:07:38 +0900

    complete

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::time::Duration;
+
+use redis::Commands;
+use swayipc::{Connection, EventType, WindowChange};
+
+fn main() -> anyhow::Result<()> {
+    let subs = [EventType::Window];
+    let conn = Connection::new()?;
+    let redis = redis::Client::open("redis://127.0.0.1")?;
+    let mut redis_con = redis.get_connection()?;
+    redis_con.set_write_timeout(Some(Duration::from_millis(500)))?;
+    let mut last = None;
+    for event in conn.subscribe(subs)? {
+        let event = match event? {
+            swayipc::Event::Window(win_event) => *win_event,
+            _ => unreachable!(),
+        };
+        if !matches!(event.change, WindowChange::Focus) {
+            continue;
+        }
+        let node_id = event.container.id;
+        log::info!("focus: {}", node_id);
+        if let Some(last) = last {
+            let _: () = redis_con.set("sway:last_focus", last)?;
+        }
+        last = Some(node_id);
+    }
+    Ok(())
+}
