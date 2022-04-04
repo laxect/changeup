ea8acab0e52fea638328911683355a2ca1e0f725
gyara <me@gyara.moe>
2022-04-04 12:01:15 +0900

    Support close event

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,55 @@
-use std::time::Duration;
+#![feature(drain_filter)]
+use std::{collections::LinkedList, time::Duration};
 
 use redis::Commands;
-use swayipc::{Connection, EventType, WindowChange};
+use swayipc::{Connection, EventType, Node, WindowChange};
 
 const SUBS: [swayipc::EventType; 1] = [EventType::Window];
+const LEN: usize = 32;
+
+fn focus(eve: Node, last: &mut LinkedList<i64>) -> anyhow::Result<()> {
+    let node_id = eve.id;
+    last.push_back(node_id);
+    if last.len() > LEN {
+        last.pop_front();
+    }
+    Ok(())
+}
+
+fn close(eve: Node, last: &mut LinkedList<i64>) -> anyhow::Result<()> {
+    let node_id = eve.id;
+    if !last.contains(&node_id) {
+        return Ok(());
+    }
+    let items = last.drain_filter(|id| *id == node_id);
+    log::info!("remove nodes: {:?}", items);
+    Ok(())
+}
 
 fn main() -> anyhow::Result<()> {
     let conn = Connection::new()?;
     let redis = redis::Client::open("redis://127.0.0.1")?;
     let mut redis_con = redis.get_connection()?;
     redis_con.set_write_timeout(Some(Duration::from_millis(500)))?;
-    let mut last = None;
+    let mut last = LinkedList::new();
     for event in conn.subscribe(SUBS)? {
         let event = match event? {
             swayipc::Event::Window(win_event) => *win_event,
             _ => unreachable!(),
         };
-        if !matches!(event.change, WindowChange::Focus) {
-            continue;
+        match event.change {
+            WindowChange::Focus => {
+                focus(event.container, &mut last)?;
+            }
+            WindowChange::Close => {
+                close(event.container, &mut last)?;
+            }
+            _ => continue,
         }
-        let node_id = event.container.id;
-        log::info!("focus: {}", node_id);
-        if let Some(last) = last {
+        dbg!(&last);
+        if let Some(last) = last.front() {
             let _: () = redis_con.set("sway:last_focus", last)?;
         }
-        last = Some(node_id);
     }
     Ok(())
 }
